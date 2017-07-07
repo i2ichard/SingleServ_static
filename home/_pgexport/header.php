829ffb50ee375402f00a5e265bfb8f68ca87c7ac
Richard Mipana <richard.mipana@gmail.com>
2017-07-06 19:34:57 -0500

    Remove un-needed files

--- home/_pgexport/header.php
+++
@@ -1,4 +0,0 @@
-<!doctype html>
-<!--  This site was created in Webflow. http://www.webflow.com -->
-<!--  Last Published: Sun Apr 23 2017 23:44:48 GMT+0000 (UTC)  -->
-<html data-wf-page="58db02570a13864876c7f0cc" data-wf-site="58db02570a13864876c7f0cb" <?php language_attributes(); ?>>
